dfcf67a573282916b2b7d7071186afdf8ff2a48a
Faseeh Hussain <syedfaseehnaqvi2@gmail.com>
2025-12-07 04:12:54 +0500

    leap yea assignment

---
+++ app.js
@@ -0,0 +1,12 @@
+let year = prompt("Enter a year");
+
+year = Number(year);
+
+// // console.log(  year);
+
+if((year % 4 === 0 && year % 100 !== 0) || ( year % 400 === 0 )){
+    alert("leap year");
+} else {
+    alert("not a leap year");
+}
+ 

23440445a7e3a089140df0da25baf8b1280ec890
Faseeh Hussain <syedfaseehnaqvi2@gmail.com>
2025-12-28 02:34:04 +0500

    leap year

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-let year = prompt("Enter a year");
+// let year = prompt("Enter a year");
+
+// year = Number(year);
+
+// // // console.log(  year);
+
+// if((year % 4 === 0 && year % 100 !== 0) || ( year % 400 === 0 )){
+//     alert("leap year");
+// } else {
+//     alert("not a leap year");
+// }
+ let year = prompt("Enter a year");
 
 year = Number(year);
 
 // // console.log(  year);
 
-if((year % 4 === 0 && year % 100 !== 0) || ( year % 400 === 0 )){
+if( year % 4 === 0  ){
     alert("leap year");
 } else {
     alert("not a leap year");
